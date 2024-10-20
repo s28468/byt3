77364ce1949716b04b4d4bf9b03ff42a36f64f59
s28305 <s28305@pjwstk.edu.pl>
2024-10-20 21:48:36 +0200

    Added serialization.

---
+++ ConsoleApp3/Serializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace ConsoleApp3;
+
+public abstract class Serializer<T>
+{
+    private const string Filename = $"{nameof(T)}.json";
+    
+    public static async Task SerializeObjects(IEnumerable<T> objects)
+    {
+        await using var stream = File.Create(Filename);
+        await JsonSerializer.SerializeAsync(stream, objects);
+    }
+    
+    public static async Task<List<T>> DeserializeObjects()
+    {
+        await using var stream = File.OpenRead(Filename);
+        return await JsonSerializer.DeserializeAsync<List<T>>(stream) ?? [];
+    }
+}
