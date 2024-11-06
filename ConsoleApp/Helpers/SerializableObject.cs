4257fa1ad65414d7b117431eafd4da326ce35332
s28305 <161834661+s28305@users.noreply.github.com>
2024-11-06 16:46:46 +0100

    Update SerializableObject.cs

--- ConsoleApp/Helpers/SerializableObject.cs
+++ ConsoleApp/Helpers/SerializableObject.cs
@@ -1,97 +1,79 @@
-﻿using System.Xml.Serialization;
+using System.Xml.Serialization;
 
-namespace ConsoleApp;
+ namespace ConsoleApp.Helpers;
 
-[Serializable]
-[XmlInclude(typeof(City))]
-[XmlInclude(typeof(Deal))]
-[XmlInclude(typeof(Resource))]
-[XmlInclude(typeof(Imported))]
-[XmlInclude(typeof(Exported))]
-[XmlInclude(typeof(ManMade))]
-[XmlInclude(typeof(Natural))]
-[XmlInclude(typeof(PublicVehicle))]
-[XmlInclude(typeof(RecreationalSpace))]
-[XmlInclude(typeof(Resident))]
-[XmlInclude(typeof(Residential))]
-[XmlInclude(typeof(Route))]
-[XmlInclude(typeof(Schedule))]
-[XmlInclude(typeof(Workplace))]
-public abstract class SerializableObject;
-
-[Serializable]
-public abstract class SerializableObject<T> : SerializableObject where T : SerializableObject<T>
-{
-    public static List<T> _instances = [];
-    private const string Filename = "Instances.xml";
-
-    public static Task<List<T>> GetAllInstances()
-    {
-        return Task.FromResult(_instances);
-    }
-
-    public static async Task SerializeAll()
-    {
-        if (_instances.Count == 0)
-        {
-            return;
-        }
-
-        var xRoot = new XmlRootAttribute
-        {
-            ElementName = "message",
-            IsNullable = true
-        };
-
-        var serializer = new XmlSerializer(typeof(List<SerializableObject>), xRoot); 
-        var combines = new List<SerializableObject>();
-
-        if (File.Exists(Filename) && new FileInfo(Filename).Length > 0)
-        {
-            using var reader = new StreamReader(Filename);
-            var existing = (List<SerializableObject>)serializer.Deserialize(reader)!;
-            combines.AddRange(existing);
-        }
-
-        combines.AddRange(_instances);
+ public abstract class SerializableObject<T>
+ {
+     private static readonly string Filename = $"{typeof(T).Name}.xml";
+     protected static List<T> _instances = [];
         
-        await using (var writer = new StreamWriter(Filename))
-        {
-            serializer.Serialize(writer, combines);
-        }
+     public static Task<List<T>> GetAllInstances()
+     {
+         return Task.FromResult(_instances);
+     }
 
-        _instances.Clear();
-    }
+     private static async Task ClearFile()
+     {
+         if (File.Exists(Filename))
+         {
+             File.Delete(Filename);
+         }
+         await using var stream = File.Create(Filename); 
+     }
 
-    public static Task LoadAll()
-    {
-        if (File.Exists(Filename) && new FileInfo(Filename).Length > 0)
-        {
-            var xRoot = new XmlRootAttribute
-            {
-                ElementName = "message",
-                IsNullable = true
-            };
+     public static async Task SerializeAll()
+     {
+         if (_instances.Count == 0)
+             return;
+         
+         var xRoot = new XmlRootAttribute
+         {
+             ElementName = "message",
+             IsNullable = true
+         };
+         var serializer = new XmlSerializer(typeof(List<T>), xRoot);
+         
+         await using (var stream = File.Create(Filename))
+         {
+             serializer.Serialize(stream, _instances);
+         }
+         
+         _instances.Clear();
+     }
 
-            var serializer = new XmlSerializer(typeof(List<SerializableObject>), xRoot); 
+     public static void LoadAll()
+     {
+         if (File.Exists(Filename) && new FileInfo(Filename).Length > 0)
+         {
+             var xRoot = new XmlRootAttribute
+             {
+                 ElementName = "message",
+                 IsNullable = true
+             };
 
-            try
-            {
-                using var reader = new StreamReader(Filename);
-                var existing = (List<SerializableObject>)serializer.Deserialize(reader)!;
-                _instances = existing as List<T> ?? []; 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error loading instances: {ex.Message}");
-                _instances = [];
-            }
-        }
-        else
-        {
-            _instances = [];
-        }
-        
-        return Task.CompletedTask;
-    }
-}
+             var serializer = new XmlSerializer(typeof(List<T>), xRoot);
+
+             try
+             {
+                 using var reader = new StreamReader(Filename);
+                 var existing = (List<T>)serializer.Deserialize(reader);
+                 
+                 if (existing != null)
+                 {
+                     _instances.AddRange(existing);
+                 }
+             }
+             catch (Exception ex)
+             {
+                 Console.WriteLine($"Error loading instances: {ex.Message}");
+                 _instances = []; 
+             }
+             
+             ClearFile();
+         }
+         else
+         {
+             _instances = []; 
+         }
+     }
+ }
